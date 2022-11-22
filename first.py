26a0a2e58937da976b7f667299184832b0efc709
Александр Чепкасов <sashadroid1@yandex.ru>
2022-11-22 23:45:45 +0500

    third

--- first.py
+++ first.py
@@ -1,3 +1,4 @@
+from PyQt5.QtCore import Qt, QPoint
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.QtGui import QPainter, QColor, QPolygon
 from PyQt5 import uic
@@ -9,13 +10,14 @@ class Window(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('circle_create.ui', self)
-        self.btn.clicked.connect(self.paint)
         self.flag = False
+        self.btn.clicked.connect(self.paint)
 
     def paintEvent(self, event):
         if self.flag:
             qp = QPainter()
             qp.begin(self)
+            qp.setBackgroundMode(1)
             self.draw_flag(qp)
             qp.end()
             self.flag = False
@@ -25,12 +27,13 @@ class Window(QMainWindow):
         self.repaint()
 
     def draw_flag(self, qp):
-        qp.setBrush(QColor(255, 255, 255))
-        qp.drawEllipse(200, 200, randint(10, 150), randint(10, 150))
+        qp.setBrush(QColor(255, 255, 0))
+        r = randint(1, 150)
+        qp.drawEllipse(200 - r // 2, 200 - r // 2, r, r)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     w = Window()
     w.show()
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
